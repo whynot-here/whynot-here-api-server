0973942447ce359785ee6c7516f18d049b955c6f
osj3474 <handong3474@gmail.com>
2022-01-31 14:26:12 +0900

    fix: queryDSL Entity 사용

--- src/main/java/handong/whynot/service/PostService.java
+++ src/main/java/handong/whynot/service/PostService.java
@@ -1,12 +1,15 @@
 package handong.whynot.service;
 
+import handong.whynot.domain.Post;
 import handong.whynot.dto.job.JobEnum;
 import handong.whynot.dto.post.PostResponseDTO;
 import handong.whynot.repository.PostQueryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -15,15 +18,21 @@ public class PostService {
     private final PostQueryRepository postQueryRepository;
 
     public List<PostResponseDTO> getPosts() {
-        List<PostResponseDTO> dtos = postQueryRepository.getPosts();
 
-        dtos.forEach(it -> {
-            it.setJobs(postQueryRepository.getJobs(it.id));
-            it.setApplicants(postQueryRepository.getApplicants(it.id));
-        });
-
-        return dtos;
+        List<Post> posts = postQueryRepository.getPosts();
+        return posts.stream()
+                .map(post -> PostResponseDTO.builder()
+                                .id(post.id)
+                                .title(post.title)
+                                .postImg(post.postImg)
+                                .createdDt(post.createdDt)
+                                .createdBy(post.createdBy)
+                                .content(post.content)
+                                .isRecruiting(post.isRecruiting)
+                                .jobs(postQueryRepository.getJobs(post.id))
+                                .applicants(postQueryRepository.getApplicants(post.id))
+                                .build()
+                        )
+                .collect(Collectors.toList());
     }
-
-
 }
