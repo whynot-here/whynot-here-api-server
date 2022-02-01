8198d2722028b5fc4af749b0375ffc1634cc6ee5
seokjae.lee <seokjae.lee@linecorp.com>
2022-02-01 14:00:00 +0900

    refactor: test code, 리뷰

---
+++ src/test/java/handong/whynot/service/PostServiceJpaTest.java
@@ -0,0 +1,45 @@
+package handong.whynot.service;
+
+import handong.whynot.domain.JobPost;
+import handong.whynot.domain.Post;
+import handong.whynot.dto.post.PostRequestDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootTest
+class PostServiceJpaTest {
+
+    @Autowired
+    private EntityManager entityManager;
+    @Autowired
+    private PostService postService;
+
+
+    @Transactional
+    @Test
+    @Rollback(value = false)
+    void createPost() {
+        PostRequestDTO requestDTO = PostRequestDTO.builder()
+                .userId(1L)
+                .content("content")
+                .postImg("postImg")
+                .jobIds(Arrays.asList(1L, 2L))
+                .build();
+
+        postService.createPost(requestDTO);
+
+        entityManager.flush();
+        entityManager.clear();
+
+        Post savedPost = entityManager.find(Post.class, 2L);
+        List<JobPost> jobPosts = savedPost.getJobPosts();
+        System.out.println(jobPosts.size());
+    }
+}
