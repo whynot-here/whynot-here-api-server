f740d0c0b0940caf91cd2791b6447cbe4bc744c3
Sangjin Oh <21500404@handong.edu>
2022-02-09 21:41:33 +0900

    feat: AWS S3 업로드 추가

---
+++ src/main/java/handong/whynot/util/ImageType.java
@@ -0,0 +1,31 @@
+package handong.whynot.util;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.tika.Tika;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ImageType {
+
+    public static final ArrayList<String> IMAGE_MIME_TYPES = new ArrayList<>(
+            Arrays.asList("image/gif", "image/jpeg", "image/png", "image/bmp")   // 허용 타입들 여기서 선언
+    );
+
+    private ImageType() {}
+
+    // 허용한 파일 타입이 맞는지 검증
+    public static Boolean isValidFileMimeType(File file, ArrayList<String> MIME_TYPES) throws IOException {
+        final String fileMimeType = new Tika().detect(file);
+
+        for (String mimeType : MIME_TYPES) {
+            if (StringUtils.equals(mimeType, fileMimeType)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
