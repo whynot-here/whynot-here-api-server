0749353b5389e95952510e6bb5b0acd5688655e5
osj3474 <handong3474@gmail.com>
2022-02-14 11:37:22 +0900

    feat: 직군 조회 API 추가

--- src/main/java/handong/whynot/api/JobController.java
+++ src/main/java/handong/whynot/api/JobController.java
@@ -1,4 +1,29 @@
 package handong.whynot.api;
 
+import handong.whynot.domain.Job;
+import handong.whynot.service.JobService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/jobs")
+@RequiredArgsConstructor
 public class JobController {
+
+    private final JobService jobService;
+
+    @GetMapping("")
+    public List<Job> getJobs() {
+        return jobService.getJobs();
+    }
+
+    @GetMapping("/{jobId}")
+    public Job getJob(@PathVariable Long jobId) {
+        return jobService.getJob(jobId);
+    }
 }
