ed2bfd42c7f86415eb77988f16884e3ee005b7ec
Sangjin Oh <21500404@handong.edu>
2022-02-14 15:32:57 +0900

    test: 댓글 조회 테스트 추가

--- src/test/java/handong/whynot/api/PostControllerTest.java
+++ src/test/java/handong/whynot/api/PostControllerTest.java
@@ -41,7 +41,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(PostController.class)
 @MockBean(JpaMetamodelMappingContext.class)
-public class PostServiceMvcTest {
+public class PostControllerTest {
 
     @MockBean private PostService postService;
     @MockBean private PostRepository postRepository;
