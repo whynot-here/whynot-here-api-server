7a1bcbfd283fdefed37d502f058a7d70bca43570
Sangjin Oh <21500404@handong.edu>
2022-02-15 15:37:57 +0900

    refactor: 도메인 -> DTO로 반환

--- src/main/java/handong/whynot/api/JobController.java
+++ src/main/java/handong/whynot/api/JobController.java
@@ -1,6 +1,7 @@
 package handong.whynot.api;
 
 import handong.whynot.domain.Job;
+import handong.whynot.dto.job.JobResponseDTO;
 import handong.whynot.service.JobService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/v1/jobs")
@@ -18,12 +20,23 @@ public class JobController {
     private final JobService jobService;
 
     @GetMapping("")
-    public List<Job> getJobs() {
-        return jobService.getJobs();
+    public List<JobResponseDTO> getJobs() {
+
+        return jobService.getJobs().stream()
+                .map(job -> JobResponseDTO.builder()
+                        .id(job.getId())
+                        .name(job.getName())
+                        .build())
+                .collect(Collectors.toList());
     }
 
     @GetMapping("/{jobId}")
-    public Job getJob(@PathVariable Long jobId) {
-        return jobService.getJob(jobId);
+    public JobResponseDTO getJob(@PathVariable Long jobId) {
+
+        Job job = jobService.getJob(jobId);
+        return JobResponseDTO.builder()
+                .id(job.getId())
+                .name(job.getName())
+                .build();
     }
 }
---
+++ src/main/java/handong/whynot/dto/job/JobResponseDTO.java
@@ -0,0 +1,14 @@
+package handong.whynot.dto.job;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@Builder
+public class JobResponseDTO {
+
+    private Long id;
+    private String name;
+
+}
