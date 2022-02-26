a44ff105cd5ac7306974c07163474c9582a58371
Sangjin Oh <21500404@handong.edu>
2022-02-26 20:21:51 +0900

    fix: import 제외 부분 수정

--- src/main/java/handong/whynot/config/SecurityConfig.java
+++ src/main/java/handong/whynot/config/SecurityConfig.java
@@ -2,12 +2,16 @@ package handong.whynot.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
