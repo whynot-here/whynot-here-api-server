36ba3918826c3558b5de12c46cda3688ea951ed4
seokjae.lee <seokjae.lee@linecorp.com>
2022-03-13 14:16:06 +0900

    feat: MethodArgumentNotValidExceptionHandler 추가

--- src/main/java/handong/whynot/api/AccountController.java
+++ src/main/java/handong/whynot/api/AccountController.java
@@ -1,19 +1,18 @@
 package handong.whynot.api;
 
-import handong.whynot.domain.Account;
-import handong.whynot.dto.account.AccountResponseCode;
-import handong.whynot.dto.account.SignUpDTO;
-import handong.whynot.dto.common.ResponseDTO;
-import handong.whynot.exception.account.AccountNotValidFormException;
-import handong.whynot.service.AccountService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.validation.Errors;
+import javax.validation.Valid;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
+import handong.whynot.domain.Account;
+import handong.whynot.dto.account.AccountResponseCode;
+import handong.whynot.dto.account.SignUpDTO;
+import handong.whynot.dto.common.ResponseDTO;
+import handong.whynot.service.AccountService;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,13 +21,7 @@ public class AccountController {
     private final AccountService accountService;
 
     @PostMapping("/sign-up")
-    public ResponseDTO signUp(@Valid @RequestBody SignUpDTO signUpDTO, Errors errors) {
-        // TODO: 22.02.20. seokjae.lee, MethodArgumentNotValidException 을 ExceptionHandler로 처리
-        // 입력 검증 (사용자)
-        if(errors.hasErrors()) {
-            throw new AccountNotValidFormException(AccountResponseCode.ACCOUNT_NOT_VALID_FROM);
-        }
-
+    public ResponseDTO signUp(@Valid @RequestBody SignUpDTO signUpDTO) {
         // Account 저장
         Account account = accountService.createAccount(signUpDTO);
 
