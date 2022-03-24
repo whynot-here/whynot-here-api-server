0c51dec528f5fcc3e70ffba016be15ae73d09406
Sangjin Oh <21500404@handong.edu>
2022-03-24 19:47:57 +0900

    fix: url 형식 통일 (케밥케이스)

--- src/main/java/handong/whynot/config/SecurityConfig.java
+++ src/main/java/handong/whynot/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
         http
                 .csrf().disable()   // 개발 단계에서만 허용
                 .authorizeRequests()
-                .antMatchers("/", "/login", "/sign-up", "/check-email-token", "/resendToken",
+                .antMatchers("/", "/login", "/sign-up", "/check-email-token", "/resend-token",
                         "/email-login", "/login-by-email").permitAll()
                 .antMatchers("/v1/posts/favorite/**", "/v1/posts/apply/**", "/v1/posts/own/**").hasRole("USER")
                 .antMatchers(HttpMethod.GET,"/v1/posts/**", "/v1/comments/**").permitAll()
