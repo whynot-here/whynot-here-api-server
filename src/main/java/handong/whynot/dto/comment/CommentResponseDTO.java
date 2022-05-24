e752a4fe1be5ce88548958623a04222b76525ec6
Sangjin Oh <handong3474@gmail.com>
2022-05-24 21:17:34 +0900

    fix: 리뷰반영 (사용자 정보 추가로 전송)

--- src/main/java/handong/whynot/dto/comment/CommentResponseDTO.java
+++ src/main/java/handong/whynot/dto/comment/CommentResponseDTO.java
@@ -1,6 +1,7 @@
 package handong.whynot.dto.comment;
 
 import handong.whynot.domain.Comment;
+import handong.whynot.dto.account.AccountResponseDTO;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -14,14 +15,15 @@ public class CommentResponseDTO {
 
     private Long commentId;
     private String content;
-    private String writer;
+    private AccountResponseDTO account;
     private LocalDateTime createdDt;
 
     public static CommentResponseDTO of(Comment comment) {
+
         return CommentResponseDTO.builder()
                 .commentId(comment.getId())
                 .content(comment.getContent())
-                .writer(comment.getCreatedBy().getNickname())
+                .account(AccountResponseDTO.of(comment.getCreatedBy()))
                 .createdDt(comment.getCreatedDt())
                 .build();
     }
