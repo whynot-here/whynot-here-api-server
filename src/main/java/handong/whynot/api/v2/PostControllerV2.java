a6df4bec370c0173ccfc9c5a777e22ac8a06b492
Sangjin Oh <handong3474@gmail.com>
2022-06-28 20:45:02 +0900

    feat: Post API v2 게시

--- src/main/java/handong/whynot/api/v2/PostControllerV2.java
+++ src/main/java/handong/whynot/api/v2/PostControllerV2.java
@@ -1,17 +1,18 @@
 package handong.whynot.api.v2;
 
+import handong.whynot.domain.Account;
+import handong.whynot.dto.common.ResponseDTO;
 import handong.whynot.dto.job.JobType;
-import handong.whynot.dto.post.PostResponseDTO;
-import handong.whynot.dto.post.RecruitStatus;
+import handong.whynot.dto.post.*;
 import handong.whynot.service.PostService;
+import handong.whynot.service.AccountSecurityService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequestMapping("/v2/posts")
@@ -19,6 +20,7 @@ import java.util.List;
 public class PostControllerV2 {
 
     private final PostService postService;
+    private final AccountSecurityService accountSecurityService;
 
     @Operation(summary = "공고 전체 조회")
     @GetMapping("")
@@ -27,5 +29,120 @@ public class PostControllerV2 {
             @RequestParam(name = "jobs", required = false, defaultValue = "") List<JobType> jobTypeList) {
 
         return postService.getPostsByParam(status, jobTypeList);
+    }
+
+    @Operation(summary = "공고 생성")
+    @PostMapping("")
+    @ResponseStatus(CREATED)
+    public ResponseDTO createPost(@RequestBody PostRequestDTO request) {
+
+        Account account = accountSecurityService.getCurrentAccount();
+        postService.createPost(request, account);
+
+        return ResponseDTO.of(PostResponseCode.POST_CREATE_OK);
+    }
+
+    @Operation(summary = "공고 단건 조회")
+    @GetMapping("/{postId}")
+    public PostResponseDTO getPost(@PathVariable Long postId) {
+        return postService.getPost(postId);
+    }
+
+    @Operation(summary = "공고 단건 삭제")
+    @DeleteMapping("/{postId}")
+    public ResponseDTO deletePost(@PathVariable Long postId) {
+
+        Account account = accountSecurityService.getCurrentAccount();
+        postService.deletePost(postId, account);
+
+        return ResponseDTO.of(PostResponseCode.POST_DELETE_OK);
+    }
+
+    @Operation(summary = "공고 단건 수정")
+    @PutMapping("/{postId}")
+    public ResponseDTO updatePost(@PathVariable Long postId, @RequestBody PostRequestDTO request) {
+
+        Account account = accountSecurityService.getCurrentAccount();
+        postService.updatePost(postId, request, account);
+
+        return ResponseDTO.of(PostResponseCode.POST_UPDATE_OK);
+    }
+
+    @Operation(summary = "‘좋아요’ 공고 전체 조회")
+    @GetMapping("/favorite")
+    public List<PostResponseDTO> getFavorite() {
+
+        Account account = accountSecurityService.getCurrentAccount();
+
+        return postService.getFavorites(account);
+    }
+
+    @Operation(summary = "‘좋아요’ 공고 단건 생성 (좋아요 on)")
+    @PostMapping("/favorite/{postId}")
+    @ResponseStatus(CREATED)
+    public ResponseDTO createFavorite(@PathVariable Long postId) {
+
+        Account account = accountSecurityService.getCurrentAccount();
+        postService.createFavorite(postId, account);
+
+        return ResponseDTO.of(PostResponseCode.POST_CREATE_FAVORITE_OK);
+    }
+
+    @Operation(summary = "‘좋아요’ 공고 단건 삭제 (좋아요 off)")
+    @DeleteMapping("/favorite/{postId}")
+    public ResponseDTO deleteFavorite(@PathVariable Long postId) {
+
+        Account account = accountSecurityService.getCurrentAccount();
+        postService.deleteFavorite(postId, account);
+
+        return ResponseDTO.of(PostResponseCode.POST_DELETE_FAVORITE_OK);
+    }
+
+    @Operation(summary = "신청한 공고 전체 조회")
+    @GetMapping("/apply")
+    public List<PostResponseDTO> getApplys() {
+
+        Account account = accountSecurityService.getCurrentAccount();
+        return postService.getApplys(account);
+    }
+
+    @Operation(summary = "신청한 공고 단건 생성 (지원) + 이메일 알림")
+    @PostMapping("/apply/{postId}")
+    @ResponseStatus(CREATED)
+    public ResponseDTO createApply(@PathVariable Long postId, @RequestBody PostApplyRequestDTO request) {
+
+        Account account = accountSecurityService.getCurrentAccount();
+        postService.createApply(postId, request, account);
+
+        return ResponseDTO.of(PostResponseCode.POST_CREATE_APPLY_OK);
+    }
+
+    @Operation(summary = "신청한 공고 단건 삭제 (지원 취소) + 이메일 알림")
+    @DeleteMapping("/apply/{postId}")
+    public ResponseDTO deleteApply(@PathVariable Long postId) {
+
+        Account account = accountSecurityService.getCurrentAccount();
+        postService.deleteApply(postId, account);
+
+        return ResponseDTO.of(PostResponseCode.POST_DELETE_APPLY_OK);
+    }
+
+    @Operation(summary = "생성한 공고 전체 조회성")
+    @GetMapping("/own")
+    public List<PostResponseDTO> getMyPosts() {
+
+        Account account = accountSecurityService.getCurrentAccount();
+
+        return postService.getMyPosts(account);
+    }
+
+    @Operation(summary = "공고 모집 상태변경")
+    @PostMapping("/own/{postId}")
+    public ResponseDTO changeRecruiting(@PathVariable Long postId, @RequestBody PostRecruitDTO request) {
+
+        Account account = accountSecurityService.getCurrentAccount();
+        postService.changeRecruiting(postId, request, account);
+
+        return ResponseDTO.of(PostResponseCode.POST_END_RECRUIT_OK);
     }
 }
