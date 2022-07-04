73f601b2a499b19acac0b4676babfa1a8beb44ac
Sangjin Oh <handong3474@gmail.com>
2022-07-05 08:06:50 +0900

    feat: path설정 위치 변경 (shouldNotFilter -> SecurityConfig)

--- src/main/java/handong/whynot/filter/CustomAuthorizationFilter.java
+++ src/main/java/handong/whynot/filter/CustomAuthorizationFilter.java
@@ -10,7 +10,6 @@ import handong.whynot.exception.account.AccountTokenExpiredException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -22,8 +21,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.util.Collections;
-import java.util.Objects;
+import java.util.List;
 
 @Slf4j
 @Component
@@ -37,31 +35,30 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
 
         String authorization = request.getHeader("Authorization");
 
-        // 토큰이 없는 경우 (Forbidden Exception)
-        if (Objects.isNull(authorization)) {
-            throw new AccountNotVerifiedException(AccountResponseCode.ACCOUNT_FORBIDDEN);
-        }
-
         try {
-            SignedJWT signedJWT = SignedJWT.parse(authorization);
+            // 인증 필요한 요청이 아닌 경우 bypass
+            if (StringUtils.isNotBlank(authorization)) {
 
-            // jwt Secret 검증
-            signedJWT.verify(new MACVerifier(jwtSecret));
+                SignedJWT signedJWT = SignedJWT.parse(authorization);
 
-            // jwt ExpiredTime 검증
-            JWTClaimsSet jwtClaimsSet = signedJWT.getJWTClaimsSet();
-            LocalDateTime tokenExpiredTime = LocalDateTime.ofInstant(jwtClaimsSet.getExpirationTime().toInstant(), ZoneOffset.UTC);
+                // jwt Secret 검증
+                signedJWT.verify(new MACVerifier(jwtSecret));
 
-            if (tokenExpiredTime.isBefore(LocalDateTime.now())) {  // 지금보다 과거이면 true
-                throw new AccountTokenExpiredException(AccountResponseCode.ACCOUNT_TOKEN_EXPIRED);
+                // jwt ExpiredTime 검증
+                JWTClaimsSet jwtClaimsSet = signedJWT.getJWTClaimsSet();
+                LocalDateTime tokenExpiredTime = LocalDateTime.ofInstant(jwtClaimsSet.getExpirationTime().toInstant(), ZoneOffset.UTC);
+
+                if (tokenExpiredTime.isBefore(LocalDateTime.now())) {  // 지금보다 과거이면 true
+                    throw new AccountTokenExpiredException(AccountResponseCode.ACCOUNT_TOKEN_EXPIRED);
+                }
+
+                String accountId = jwtClaimsSet.getSubject();
+                UserAccount userAccount = new UserAccount(accountId);
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                        new UsernamePasswordAuthenticationToken(userAccount, null, List.of(new SimpleGrantedAuthority("ROLE_USER")));
+
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
             }
-
-            String accountId = jwtClaimsSet.getSubject();
-            UserAccount userAccount = new UserAccount(accountId);
-            UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
-                    new UsernamePasswordAuthenticationToken(userAccount, null, Collections.singletonList(new SimpleGrantedAuthority("USER")));
-
-            SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
 
             filterChain.doFilter(request, response);
 
@@ -70,31 +67,7 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
             throw new AccountTokenExpiredException(AccountResponseCode.ACCOUNT_TOKEN_EXPIRED);
         } catch (Exception e) {
             // 비정상 토큰인 경우
-            throw new AccountNotVerifiedException(AccountResponseCode.ACCOUNT_TOKEN_EXPIRED);
+            throw new AccountNotVerifiedException(AccountResponseCode.ACCOUNT_FORBIDDEN);
         }
-    }
-
-    /**
-     * JWT 토큰 검증 예외 화이트리스트
-     *
-     * @param request
-     * @return
-     */
-    @Override
-    protected boolean shouldNotFilter(HttpServletRequest request) {
-        String path = request.getRequestURI();
-
-        return StringUtils.equals(path, "/")
-                // Account
-                || StringUtils.equals(path, "/v2/sign-in")
-
-                // Post
-                || (StringUtils.equals(path, "/v2/posts") && HttpMethod.GET.matches(request.getMethod()))
-                || (path.matches("^/v2/posts/\\d+$") && HttpMethod.GET.matches(request.getMethod()))
-
-                // Swagger
-                || path.matches("^/swagger-ui/.*")
-                || path.matches("^/v3/api-docs.*")
-                ;
     }
 }
