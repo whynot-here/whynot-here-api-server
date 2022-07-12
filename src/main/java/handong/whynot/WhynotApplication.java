ca074197a549489ae2f4e32b6a2d78c51791e947
seokjae.lee <seokjae.lee@linecorp.com>
2022-07-12 16:13:03 +0900

    feat: 테스트 클래스에 JpaMetamodelMappingContext 설정 제거

--- src/main/java/handong/whynot/WhynotApplication.java
+++ src/main/java/handong/whynot/WhynotApplication.java
@@ -8,11 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Slf4j
-@EnableJpaAuditing
 @SpringBootApplication
 public class WhynotApplication {
 
