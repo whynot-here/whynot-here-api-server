c53a2b414c6a3cc8911ce6c1a0c338ebfc5f3b9a
osj3474 <handong3474@gmail.com>
2022-07-22 22:11:51 +0900

    feat: 구글 로그인 DTO 생성 및 응답코드 생성

--- src/main/java/handong/whynot/dto/account/UserAccount.java
+++ src/main/java/handong/whynot/dto/account/UserAccount.java
@@ -2,24 +2,40 @@ package handong.whynot.dto.account;
 
 import handong.whynot.domain.Account;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.List;
+import java.util.Map;
 
 @Getter
-public class UserAccount extends User {
+@Setter
+public class UserAccount extends User implements OAuth2User {
 
     private Account account;
     private String accountId;
+    private Map<String, Object> attributes;
 
     public UserAccount(Account account) {
-        super(account.getNickname(), account.getPassword(), List.of(new SimpleGrantedAuthority("ROLE_USER")));
+        super(account.getNickname(), account.getPassword() == null ? "" : account.getPassword(), List.of(new SimpleGrantedAuthority("ROLE_USER")));
         this.account = account;
     }
 
     public UserAccount(String accountId) {
         super(accountId, "", List.of(new SimpleGrantedAuthority("ROLE_USER")));
         this.accountId = accountId;
+    }
+
+    @Override
+    public String getName() {
+        return accountId;
+    }
+
+    public static UserAccount create(Account account, Map<String, Object> attributes) {
+        UserAccount userAccount = new UserAccount(account);
+        userAccount.setAttributes(attributes);
+        return userAccount;
     }
 }
