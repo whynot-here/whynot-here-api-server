c96f0a5baf5b802b6610741b9bb8bb9a67debd71
osj3474 <handong3474@gmail.com>
2022-07-23 13:22:15 +0900

    fix: 불필요 import 제거

--- src/main/java/handong/whynot/handler/CustomExceptionHandler.java
+++ src/main/java/handong/whynot/handler/CustomExceptionHandler.java
@@ -5,12 +5,9 @@ import handong.whynot.dto.common.ResponseCode;
 import handong.whynot.exception.AbstractBaseException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
-
-import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Slf4j
 @RestControllerAdvice
