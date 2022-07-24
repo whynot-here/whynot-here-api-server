2b7d820e98fa413e61c41b71340a132a44792742
doljae <seok9211@naver.com>
2022-07-24 10:53:18 +0900

    add test cases

--- src/main/java/handong/whynot/util/NickNameMaker.java
+++ src/main/java/handong/whynot/util/NickNameMaker.java
@@ -1,6 +1,9 @@
 package handong.whynot.util;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.apache.commons.lang3.RandomUtils;
 
@@ -12,5 +15,26 @@ public final class NickNameMaker {
 
     public static String make(String nickname) {
         return PREFIX_LIST.get(RandomUtils.nextInt(0, PREFIX_LIST.size() - 1)) + nickname;
+    }
+
+    public static void main(String[] args) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int threshold = 100;
+
+        for (int i = 0; i < 100000; i++) {
+            int currentKey = RandomUtils.nextInt(0, threshold);
+            if (map.containsKey(currentKey)) {
+                map.put(currentKey, map.get(currentKey) + 1);
+            } else {
+                map.put(currentKey, 1);
+            }
+        }
+
+        int sum = 0;
+        for (Entry<Integer, Integer> entry : map.entrySet()) {
+            System.out.println("key: " + entry.getKey() + " value: " + entry.getValue());
+            sum += entry.getValue();
+        }
+        System.out.println(sum);
     }
 }
