c3fb91e1ae43d271ed018d10df0373b2a9d699a7
osj3474 <handong3474@gmail.com>
2022-09-15 20:27:23 +0900

    feat: 기본 카테고리 조회 redis 적용

--- src/main/java/handong/whynot/api/v2/CategoryController.java
+++ src/main/java/handong/whynot/api/v2/CategoryController.java
@@ -3,6 +3,7 @@ package handong.whynot.api.v2;
 import handong.whynot.dto.category.CategoryInfo;
 import handong.whynot.service.CategoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @GetMapping("/default")
+    @Cacheable(value="DefaultCategory")
     public List<CategoryInfo> getDefaultList() {
 
         return categoryService.getDefaultList();
---
+++ src/main/java/handong/whynot/config/RedisConfiguration.java
@@ -0,0 +1,30 @@
+package handong.whynot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfiguration {
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(lettuceConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplateObj(LettuceConnectionFactory lettuceConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(lettuceConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+        return redisTemplate;
+    }
+}
