616a232db3288ce9c530955f3e83b315ae6493a8
osj3474 <handong3474@gmail.com>
2022-10-16 10:48:25 +0900

    refactor: local 레디스 설정 제외

--- src/main/java/handong/whynot/config/RedisConfiguration.java
+++ src/main/java/handong/whynot/config/RedisConfiguration.java
@@ -2,12 +2,14 @@ package handong.whynot.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@Profile("!local")
 public class RedisConfiguration {
 
     @Bean
