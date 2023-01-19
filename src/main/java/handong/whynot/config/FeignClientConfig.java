5e7e0a0c7151d9048bc57948f090f90ec8b76f91
osj3474 <handong3474@gmail.com>
2023-01-19 21:34:44 +0900

    setting: feign 설정 보완

--- src/main/java/handong/whynot/config/FeignClientConfig.java
+++ src/main/java/handong/whynot/config/FeignClientConfig.java
@@ -1,12 +1,30 @@
 package handong.whynot.config;
 
+import feign.Logger;
+import feign.Retryer;
 import feign.codec.Encoder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+import java.util.concurrent.TimeUnit;
+
+@Configuration
 public class FeignClientConfig {
 
   @Bean
   Encoder formEncoder() {
     return new feign.form.FormEncoder();
+  }
+
+  @Bean
+  Retryer.Default retryer() {
+    return new Retryer.Default(TimeUnit.SECONDS.toMillis(1L), TimeUnit.SECONDS.toMillis(3L), 5);
+  }
+
+  @Bean
+  @Profile("local")
+  Logger.Level feignLoggerLevel() {
+    return Logger.Level.FULL;
   }
 }
