b13e13a4b2539481ccfea2c137919be982aa65f5
osj3474 <handong3474@gmail.com>
2023-10-20 00:32:31 +0900

    feat: 댓글 반환 포맷 변경

--- src/main/java/handong/whynot/dto/comment/CommentResponseDTO.java
+++ src/main/java/handong/whynot/dto/comment/CommentResponseDTO.java
@@ -1,5 +1,10 @@
 package handong.whynot.dto.comment;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import handong.whynot.domain.Comment;
 import handong.whynot.dto.post.PostWriterDTO;
 import lombok.*;
@@ -13,9 +18,15 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class CommentResponseDTO {
 
+    private static final String DATE_PATTERN = "yyyy-MM-dd HH:mm";
+
     private Long commentId;
     private String content;
     private PostWriterDTO account;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATE_PATTERN, timezone = "Asia/Seoul")
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime createdDt;
 
     public static CommentResponseDTO of(Comment comment) {
